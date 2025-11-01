b65a9117812e0713f608d1a97b70cd4808c60a4b
mazhar <mazharopal@gmail.com>
2025-11-01 15:04:54 +0500

    initial data

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -144,6 +144,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    'authsandtasks.pythonanywhere.com'
 ]
 
 STATICFILES_DIRS = [
